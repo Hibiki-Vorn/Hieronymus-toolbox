4a030287489b5e931a1f9040d25132b687fa8b7d
Hieronymus <hieronymus@git.com>
2025-11-09 05:37:45 +0800

    add webgl detect of live2D

--- src/lib/live2D.svelte
+++ src/lib/live2D.svelte
@@ -1,3 +1,9 @@
+<script>
+    const canvas = document.createElement("canvas");
+    const gl = canvas.getContext("webgl2") || canvas.getContext("webgl");
+    if (!gl) alert("你的浏览器不支持 WebGL，无法显示 Live2D 模型");
+</script>
+
 <!-- svelte-ignore a11y_missing_attribute -->
 <!-- svelte-ignore element_invalid_self_closing_tag -->
 <iframe src="https://live2d-widget-5o0.pages.dev/" frameborder="0"/>
